e30b8ad61bd3f030064c8db9e63090d24b7306a3
Minnie <261674283+belphia-ai@users.noreply.github.com>
2026-02-14 23:31:12 +0000

    Make Vite base path dynamic

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hello-world/',
+  base: process.env.VERCEL ? '/' : '/hello-world/',
   plugins: [react()],
 })
